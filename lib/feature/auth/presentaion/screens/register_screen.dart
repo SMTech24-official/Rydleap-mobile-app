d1c22b465321546a80a2fc66c2b07206fd41c4fd
alphzen01 <parvejmahmud555@gmail.com>
2024-09-08 11:29:58 +0600

    ..

--- lib/feature/auth/presentaion/screens/register_screen.dart
+++ lib/feature/auth/presentaion/screens/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rydleap/core/app_icons.dart';
 import 'package:rydleap/core/app_imagese.dart';
@@ -10,6 +12,8 @@ import 'package:rydleap/core/global_widgets/global_variable.dart';
 import 'package:rydleap/core/global_widgets/phone_input.dart';
 import 'package:rydleap/core/utility/app_colors.dart';
 import 'package:rydleap/feature/auth/components/or_sign_in_with.dart';
+import 'package:rydleap/feature/auth/dirver_registration/driver_registration.dart';
+import 'package:rydleap/feature/auth/presentaion/screens/change_password.dart';
 import 'package:rydleap/feature/auth/presentaion/screens/name_email_screen.dart';
 import 'package:rydleap/feature/notifications_access/notifications_access_screen.dart';
 import 'package:rydleap/feature/profile/screen/profile_screen.dart';
@@ -67,7 +71,9 @@ class RegisterScreen extends StatelessWidget {
           children: [
             SizedBox(height: getHeight(20),),
             CustomPhoneInput(),
-            CustomGradientButton(text: "Get OTP", onTap: () {}),
+            CustomGradientButton(text: "Get OTP", onTap: () {
+              Get.to(DriverRegistrationScreen());
+            }),
             Container(
               
               width: getWidth(120),
