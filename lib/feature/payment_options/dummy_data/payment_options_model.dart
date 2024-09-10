1a86d29f68a94df36356dd8f6fd68e2248dfcf17
yasinpalash <yasinmiapolash@gmail.com>
2024-09-10 04:50:39 -0700

    work with payment_options_screen.dart

--- lib/feature/payment_options/dummy_data/payment_options_model.dart
+++ lib/feature/payment_options/dummy_data/payment_options_model.dart
@@ -6,29 +6,48 @@ import 'package:rydleap/feature/payment_options/widgets/custom_payment_method_sc
 import '../../../core/app_imagese.dart';
 
 class PaymentOptionsModel {
-  final String icon,title;
+  final String icon, title;
   final VoidCallback onTap;
-  PaymentOptionsModel( {required this.icon, required this.title,required this.onTap});
+  PaymentOptionsModel(
+      {required this.icon, required this.title, required this.onTap});
 }
 
-
-List <PaymentOptionsModel>paymentItems=[
+List<PaymentOptionsModel> paymentItems = [
   PaymentOptionsModel(
       icon: AppImagese.debitAndCreditCard,
-      title: "Debit Card or Credit Card", onTap: () {  }
-  ),
+      title: "Debit Card or Credit Card",
+      onTap: () {}),
   PaymentOptionsModel(
       icon: AppImagese.paypal,
-      title: "Paypal", onTap: () {
-        Get.to(CustomPaymentMethodScreen());
-  }
-  ),
+      title: "Paypal",
+      onTap: () {
+        Get.to(CustomPaymentMethodScreen(
+          title: 'Pay Pal',
+          subtitle: 'Use Pay Pal for quick and secure payments',
+          imagePath: AppImagese.payPalLarge,
+          bottomText: 'Pay Pal',
+        ));
+      }),
   PaymentOptionsModel(
       icon: AppIcons.appleIcon,
-      title: "Apple Pay", onTap: () {  }
-  ),
+      title: "Apple Pay",
+      onTap: () {
+        Get.to(CustomPaymentMethodScreen(
+          title: 'Pay Pal',
+          subtitle: 'Use Pay Pal for quick and secure payments',
+          imagePath: AppImagese.payPalLarge,
+          bottomText: 'Apple Pal',
+        ));
+      }),
   PaymentOptionsModel(
       icon: AppIcons.googleIcon,
-      title: "Google Pay", onTap: () {  }
-  ),
+      title: "Google Pay",
+      onTap: () {
+        Get.to(CustomPaymentMethodScreen(
+          title: 'Pay Pal',
+          subtitle: 'Use Pay Pal for quick and secure payments',
+          imagePath: AppImagese.payPalLarge,
+          bottomText: 'Google Pay',
+        ));
+      }),
 ];
