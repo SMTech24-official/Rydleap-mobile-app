7d63367e786ae5c9119a0da56c0b2c2f5ea0a14e
Nafim Ahmed <recentnafimahmed@gmail.com>
2024-09-13 11:10:53 +0600

    notification widget created

--- lib/feature/driver_dashboard/presentation/driver_dashboard.dart
+++ lib/feature/driver_dashboard/presentation/driver_dashboard.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rydleap/core/app_imagese.dart';
 import 'package:rydleap/core/app_sizes.dart';
+import 'package:rydleap/core/global_widgets/notification_tile.dart';
 import 'package:rydleap/feature/driver_ride_request/presentation/driver_ride_request.dart';
 import 'package:rydleap/feature/profile/screen/profile_screen.dart';
 
@@ -315,37 +316,7 @@ class DriverDashboard extends StatelessWidget {
                 itemCount: 4,
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 7, horizontal: 20),
-                    padding: EdgeInsets.symmetric(vertical: 13, horizontal: 5),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white.withOpacity(0.3)),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          AppImagese.profileImage,
-                          width: 43,
-                          height: 43,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Congratulations!',
-                              style: GoogleFonts.nunito(
-                                  fontSize: 13, fontWeight: FontWeight.w500),
-                            ),
-                            Text(
-                              'You got 5 star from Burkina Faso 1min',
-                              style: GoogleFonts.nunito(
-                                  fontSize: 13, fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  );
+                  return NotificationTile();
                 })
           ],
         ),
