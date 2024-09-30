c81a23c6d490728c169b41a724a840949a31bc6d
Nafim Ahmed <recentnafimahmed@gmail.com>
2024-09-30 11:39:23 +0600

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   // Lock orientation to portrait mode
+  Get.put(MapController());
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
