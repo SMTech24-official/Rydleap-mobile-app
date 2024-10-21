e7009a5dd50b724e6def6d57ca4b2397ee816af8
alphzen01 <parvejmahmud555@gmail.com>
2024-10-21 10:22:29 +0600

    ...

--- lib/core/api_url.dart
+++ lib/core/api_url.dart
@@ -1,9 +1,10 @@
 class ApiUrl {
-  static const baseUrl = "https://ryd-leap-backend.vercel.app";
+  static const baseUrl = "https://rydleap-backend-eight.vercel.app/api/v1";
 
   static const otpUrl = "$baseUrl/api/v1/otp/send-otp";
   static const verifyOtpUrl = "$baseUrl/api/v1/otp/verify-otp";
   static const userRegistrationUrl = "$baseUrl/api/v1/users/register";
   static const driverRegistrationUrl = "$baseUrl/api/v1/users/create-rider";
   static const loginUrl = "$baseUrl/api/v1/auth/login";
+  static const updateToken = "$baseUrl/api/v1/auth/update-fcp/";
 }
